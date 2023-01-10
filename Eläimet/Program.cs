25c31692618ad15e7cbfff03e874f0804a023c9d
Ilari Kokkonen <ilari.kokkonen@student.careeria.fi>
2023-01-10 12:32:43 +0200

    ertgyhb

---
+++ Eläimet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eläimet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    public static int TotalAnimals { get; set; }
+        public virtual string Sound { get; set; }
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("This animal makes a generic sound.");
+        }
+        public Animals()
+        {
+            TotalAnimals++;
+        }
+    }
+}
